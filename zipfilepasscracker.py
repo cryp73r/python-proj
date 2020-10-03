8dcca7ed478c866e50c3e96bca49320657edba2c
vishal707 <vishaljuneja707@gmail.com>
2020-10-03 20:48:08 +0530

    Create zipfilepasscracker.py
    Made this program to crack zip file passes 
    
    import sys
    import zipfile
    
    
    if (len(sys.argv) == 1) or (len(sys.argv) == 2):
        print("Usage : python3 zipfile.py <wordlist> <zipfile>")
    if len(sys.argv) == 3:
        file = sys.argv[1]
        zf = zipfile.ZipFile(sys.argv[2])
        f = open(file)
        for words in f.readlines():
            pwd = words.strip()
            try:
                zf.extractall(pwd = str.encode(pwd))
                print("Password found: {}".format(pwd))
                exit(0)
            except:
                continue

---
+++ zipfilepasscracker.py
@@ -0,0 +1,18 @@
+import sys
+import zipfile
+
+
+if (len(sys.argv) == 1) or (len(sys.argv) == 2):
+    print("Usage : python3 zipfile.py <wordlist> <zipfile>")
+if len(sys.argv) == 3:
+    file = sys.argv[1]
+    zf = zipfile.ZipFile(sys.argv[2])
+    f = open(file)
+    for words in f.readlines():
+        pwd = words.strip()
+        try:
+            zf.extractall(pwd = str.encode(pwd))
+            print("Password found: {}".format(pwd))
+            exit(0)
+        except:
+            continue
