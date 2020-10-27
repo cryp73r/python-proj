8f2fd71b5f996d233dca543d40c1af629a07cb82
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-27 20:03:55 +0530

    Update and rename main.py to SendEmails.py

--- SendEmails.py
+++ SendEmails.py
@@ -1,18 +1,21 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+#taking credentials
 email = input("Enter your e-mail address: \n")
 password = input("Enter your password: \n")
 senderadd = email
+#storing Multiple reciptents addresses
 lst = []
 reciveradd = lst.append(input("Enter Receiver's email Address: \n"))
 msg = MIMEMultipart()
+#taking messages
 msg['To'] = ",".join(reciveradd)
 msg['From'] = senderadd
 msg['Subject'] = input("Enter subject: \n")
 body = input("Enter Message: \n")t'
 msg.attach(MIMEText(body,'plain'))
-
+#connecting to gmail
 mail = smtplib.SMTP('smtp.gmail.com',2525)
 mail.ehlo()
 mail.starttls()
